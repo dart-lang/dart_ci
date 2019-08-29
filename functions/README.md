20e6bc4ca1d79df5f2febb594494492ef4e53bc8
William Hesse <whesse@google.com>
2019-08-29 13:44:43 +0000

    Initial commit of cloud_functions that copy results from Pub/Sub to Firestore
    Change-Id: Ieae21297e41e9152eda11766cf567f820519003b
    Reviewed-on: https://dart-review.googlesource.com/c/dart_ci/+/111040
    Reviewed-by: Alexander Thomas <athom@google.com>

---
+++ functions/README.md
@@ -0,0 +1,18 @@
+# Cloud Functions for Dart CI
+
+Dart test results are sent to Pub/Sub and stored in Firestore
+by these functions, written in Dart and compiled to NodeJS.
+
+These functions write to the document collections 'results' and 'commits'
+in the projects 'dart-ci' and 'dart-ci-staging'
+
+## Installation
+
+Run "pub get"
+Run "npm install"
+Run "pub run build_runner build --output=build"
+Run "firebase -P dart-ci-staging deploy --only functions"
+
+
+
+
---
+++ functions/node/test.dart
@@ -0,0 +1,43 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+
+import 'index.dart';
+
+String resultJson = '''
+{
+"name":"dart2js_extra/local_function_signatures_strong_test/none",
+"configuration":"dart2js-new-rti-linux-x64-d8",
+"suite":"dart2js_extra",
+"test_name":"local_function_signatures_strong_test/none",
+"time_ms":2384,
+"result":"Pass",
+"expected":"Pass",
+"matches":true,
+"bot_name":"luci-dart-try-xenial-70-8fkh",
+"commit_hash":"9cd47ac2e6ac024e5a0fd1d5667d94a8c2fd2d5e",
+"commit_time":1563576771,
+"build_number":"307",
+"builder_name":"dart2js-rti-linux-x64-d8",
+"flaky":false,
+"previous_flaky":false,
+"previous_result":"RuntimeError",
+"previous_commit_hash":"ebc180be95efd89b8745ddffcedf970af6e36dc1",
+"previous_commit_time":1563576211,
+"previous_build_number":"306",
+"changed":true
+}
+''';
+
+void main() async {
+  print("starting test");
+  Map<String, dynamic> result = jsonDecode(resultJson);
+  final stats = Statistics();
+  final info = await storeBuildInfo([result], stats);
+  print("info: $info");
+  stats.report();
+  await storeChange(result, info, stats);
+  stats.report();
+}
