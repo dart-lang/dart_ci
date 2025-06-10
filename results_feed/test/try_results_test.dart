8573a62d1b89d0374457f7c16dfb09fbf229fbd5
Jake Macdonald <jakemac@google.com>
2025-06-10 20:56:17 +0000

    add workflow for results_feed

--- results_feed/test/try_results_test.dart
+++ results_feed/test/try_results_test.dart
@@ -3,6 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 @TestOn('browser')
+@Tags(['requires_auth'])
 import 'package:angular/di.dart';
 import 'package:angular_test/angular_test.dart';
 import 'package:dart_results_feed/src/components/try_results_component.dart';
