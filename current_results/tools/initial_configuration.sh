2bceb69d03d63c9bcfbe19a149050660ee7d3fd1
Alexander Thomas <athom@google.com>
2026-02-27 12:34:25 +0100

    [current_results] Migrate to Cloud Run (#201)
    This should avoid reports like b/488148329.
    
    * Update ESP tag.
    * Update configuration and deployment scripts.
    * Use a similar configuration to e2-small for the backend.
    * Limit backend to exactly 1 instance (it keeps results in memory).

--- current_results/tools/initial_configuration.sh
+++ current_results/tools/initial_configuration.sh
@@ -27,28 +27,23 @@ gcloud services enable compute.googleapis.com
 gcloud services enable run.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 
-gcloud compute project-info add-metadata \
-  --metadata google-compute-default-region=us-central1,google-compute-default-zone=us-central1-a
-
-gcloud compute instances create current-results-server \
-  --image-project cos-cloud --image-family=cos-117-lts --machine-type=e2-small \
-  --zone us-central1-a --metadata google-logging-enabled=true
-
-# This command requires the organization policy
-# constraints/compute.vmCanIpForward
-# The service cannot be deployed in a project in an organization that
-# does not allow VM IP forwarding
-gcloud compute networks vpc-access connectors create cloud-run-to-gce \
-  --network default --region us-central1 --range 10.126.0.0/28
-
-echo "This command should output 'state:READY'"
-
-# Reserve the cloud run hostname for the service.
+# Reserve the cloud run hostname for the backend service.
 # We need this generated hostname for later deployment.
-gcloud run deploy current-results --image="gcr.io/cloudrun/hello" \
-  --allow-unauthenticated --platform managed --region us-central1
+gcloud run deploy current-results-backend \
+  --image="gcr.io/cloudrun/hello" \
+  --allow-unauthenticated \
+  --platform managed \
+  --region us-central1 \
+  --use-http2 \
+  --cpu=1 \
+  --memory=2Gi
 
-gcloud compute ssh current-results-server --zone=us-central1-a \
-  --command="docker network create --driver bridge bridge_net; docker-credential-gcr configure-docker"
+# Reserve the cloud run hostname for the ESPv2 proxy service.
+# We need this generated hostname for later deployment.
+gcloud run deploy current-results \
+  --image="gcr.io/cloudrun/hello" \
+  --allow-unauthenticated \
+  --platform managed \
+  --region us-central1
 
 # TODO Add a call to Fetch running every 5 minutes (with POST) to cloud scheduler.
